0029ba20bf85ca08d41efaa5ca7bde1bb43859fa
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-06-06 08:24:18 +0300

    created few requestAnims, interesting, but hard to understand

--- js/app.js
+++ js/app.js
@@ -2,3 +2,16 @@ import { isWebp } from './components/isWebp.js'
 
 isWebp()
 
+const $box = document.querySelector('.box')
+
+let startTime = new Date().getTime()
+function animate() {
+	let currentTime = new Date().getTime()
+	let newPosX = 50 + ((currentTime - startTime) / 1000) * 60
+	$box.style.left = `${newPosX}px`
+
+	if (newPosX < 500) {
+		window.requestAnimationFrame(animate)
+	}
+}
+window.requestAnimationFrame(animate)
